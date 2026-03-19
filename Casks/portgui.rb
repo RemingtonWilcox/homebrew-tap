d6cb3d1f879f0438c62aa42e80e7592923e50dff
Remington <129909388+RemingtonWilcox@users.noreply.github.com>
2026-03-19 06:15:34 +0200

    Update PortGUI cask SHA for notarized build

--- Casks/portgui.rb
+++ Casks/portgui.rb
@@ -1,6 +1,6 @@
 cask "portgui" do
   version "0.1.0"
-  sha256 "84750f93bd2945ea08489b16216d18e500dbc4e6ebf831196183559ca069de38"
+  sha256 "a8a77d9cec4c44166222decccdfb6b5d79b5ab82ce4109d06ef6141540afe1a6"
 
   url "https://github.com/RemingtonWilcox/portgui/releases/download/v#{version}/PortGUI_#{version}_aarch64.dmg"
   name "PortGUI"
